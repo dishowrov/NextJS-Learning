7e7e80352ea33b2573006995b834f96904fe2986
din-islam-sh1 <dinislamshowrov.2001@gmail.com>
2024-02-03 17:35:30 +0600

    nested routes

---
+++ src/app/testimonial/marketplace/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Marketplace() {
+  return (
+    <div>
+      <h2>
+        Collected from freelance Marketplaces
+      </h2>
+    </div>
+  )
+}
---
+++ src/app/testimonial/out-market/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function OutOfMarketplace() {
+  return (
+    <div>
+      <h2>
+        Collected from out of the freelace Marketplaces. that means local.
+      </h2>
+    </div>
+  )
+}
