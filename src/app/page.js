a106445fada8bf68be35b3aa433000edc25e8371
din-islam-sh1 <dinislamshowrov.2001@gmail.com>
2024-02-04 15:43:10 +0600

    GSAP animation is working

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Gsap from "./components/Gsap";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <About/>
+      {/* <About/> */}
+      <Gsap/>
       <Footer />
     </>
   );
