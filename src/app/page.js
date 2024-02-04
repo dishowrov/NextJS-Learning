237232ded16dc7df1d2319c9a929f7c877b0e358
din-islam-sh1 <dinislamshowrov.2001@gmail.com>
2024-02-04 15:12:47 +0600

    page for gsap practicing

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <About/>
       <Footer />
     </>
   );
