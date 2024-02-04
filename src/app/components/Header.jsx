717fdd1e6938815a59a384234d9932dbee0d380b
din-islam-sh1 <dinislamshowrov.2001@gmail.com>
2024-02-04 11:45:32 +0600

    header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import siteLogo from "/src/app/img/logo-dark.png";
 
 function Header() {
     return (
         <header>
             <div className="bg-purple-50">
-
                 <div className="container m-auto">
+                    <div className="flex justify-between">
 
-                    {/* Left */}
-                    <div>
-                        <Image src={siteLogo} alt="Website Logo" />
+                        {/* Left */}
+                        <div>
+                            <Image src={siteLogo} alt="Website Logo" />
+                        </div>
+
+                        {/* Right */}
+                        <div>
+                            <ul>
+                                <li>
+                                    <Link href={"#"}> Home </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={"#"}> About </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={"#"}> Services </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={"#"}> Portfolio </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={"#"}> Experiences </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={"#"}> Contact Us </Link>
+                                </li>
+
+                            </ul>
+                        </div>
+
                     </div>
-
                 </div>
             </div>
         </header>
