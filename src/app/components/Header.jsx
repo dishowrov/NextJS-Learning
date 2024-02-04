0761b767cec33855720b8b07758643a52c4183d6
din-islam-sh1 <dinislamshowrov.2001@gmail.com>
2024-02-04 11:16:30 +0600

    paused

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import Image from 'next/image';
-import siteLogo from '/src/app/img/logo-dark.png';
+import React from "react";
+import Image from "next/image";
+import siteLogo from "/src/app/img/logo-dark.png";
 
 function Header() {
-  return (
-    <header>
+    return (
+        <header>
+            <div className="bg-purple-50">
 
-        {/* Left */}
-        <div>
-            <Image
-                src={siteLogo}
-                alt='Website Logo'
-                
-            />
-        </div>
-      
-    </header>
-  )
+                <div className="container m-auto">
+
+                    {/* Left */}
+                    <div>
+                        <Image src={siteLogo} alt="Website Logo" />
+                    </div>
+
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
